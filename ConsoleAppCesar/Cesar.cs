8ea7f2353916a5b34b1a23b807052f8533adee74
Jim Legrandhenri <jimlegrandhenri007@outlook.com>
2022-08-05 14:37:30 +0200

    Le code césar

---
+++ ConsoleAppCesar/Cesar.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppCesar
+{
+    public class Cesar
+    {
+        const int CODE_ASCII_A_MINUSCULE = 97;
+        const int LONGUEUR_ALPHABET = 26;
+        static readonly char[] alphabet;
+        static int limiteSuperieure = CODE_ASCII_A_MINUSCULE + LONGUEUR_ALPHABET;
+
+        // Ceci est un constructeur de classe
+        static Cesar()
+        {
+            alphabet = new char[LONGUEUR_ALPHABET];
+            for (int lettre = CODE_ASCII_A_MINUSCULE, cpt = 0; lettre < limiteSuperieure; lettre++, cpt++)
+            {
+                alphabet[cpt] = (char)lettre;
+            }
+        }
+
+        public static string Chiffrer(string pTexteAChiffrer, int pDecalage)
+        {
+            string texteAChiffrer = pTexteAChiffrer.ToLower();
+
+            // Traitement décalage
+            int decalage = pDecalage;
+            decalage = decalage % LONGUEUR_ALPHABET;
+            if (decalage < 0)
+            {
+                decalage += LONGUEUR_ALPHABET;
+            }
+            StringBuilder result = new StringBuilder();
+            foreach (char ch in texteAChiffrer)
+            {
+                int index = Array.IndexOf(alphabet, ch, 0);
+
+                if (index >= 0)
+                {
+                    char cryptedChar = alphabet[(index + decalage) % LONGUEUR_ALPHABET];
+                    result.Append(cryptedChar);
+                }
+                else
+                {
+                    result.Append(ch);
+                }
+            }
+            return result.ToString();
+        }
+    }
+}
---
+++ ConsoleAppCesar/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+// string[] alphabet = new string[26] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+using ConsoleAppCesar;
+using System.Text;
+
+Console.WriteLine("Tapez la chaine à chiffrer");
+string chaineAChiffrer = Console.ReadLine().ToLower();
+
+Console.WriteLine("Quel est le décalage désiré ?");
+int decalage; 
+Boolean resultTry  = Int32.TryParse(Console.ReadLine(), out decalage);
+if (!resultTry)
+{
+    Console.WriteLine("taper un nombre correct");
+    Console.Read();
+    return;
+}
+
+// le decalage est positif et compris entre 0 et 25
+
+string result = Cesar.Chiffrer(chaineAChiffrer, decalage);
+Console.WriteLine(result.ToString());
+Console.Write("\n");
+
+Console.ReadKey();
+
+// programmer la creation du tableau et puis le remplissage
+// et puis l'affichage du contenu du tableau 
+
+
+
+// decalage positif de 1  A => B ,    E => F
+// decalage négatif de -1 B => A ,    E => D
+// decalage positif de 25 B => A ,    E => D
+
+// traitement sur decalage
+// Si decalage est positif rien faire
+// Si decalage est negatif decalage = decalage + 26 exemple -1 devient +25
+
+// Si decalage est -261 => -261 modulo 26 => -1
